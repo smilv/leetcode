b2e308622228d645a2ae6c0cb6d3fea36c231a96
smilv <44606272+smilv@users.noreply.github.com>
2025-02-17 18:14:27 +0800

    88

--- 88.合并两个有序数组.js
+++ 88.合并两个有序数组.js
@@ -58,3 +58,23 @@ var merge = function (nums1, m, nums2, n) {
     nums1[i] = res[i];
   }
 };
+
+// 逆向双指针，每次取两者之中的较大者放进 nums1的最后面。
+var merge = function (nums1, m, nums2, n) {
+  let p1 = m - 1,
+    p2 = n - 1,
+    index = m + n - 1,
+    cur;
+  while (p1 >= 0 || p2 >= 0) {
+    if (p1 === -1) {
+      cur = nums2[p2--];
+    } else if (p2 === -1) {
+      cur = nums1[p1--];
+    } else if (nums1[p1] > nums2[p2]) {
+      cur = nums1[p1--];
+    } else {
+      cur = nums2[p2--];
+    }
+    nums1[index--] = cur;
+  }
+};
